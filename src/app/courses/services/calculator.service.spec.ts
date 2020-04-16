c8765f3fef4ff60fb90d483d9125565b06bba8e9
Wojciech Dudek <wdudek82@gmail.com>
2020-04-16 21:27:39 +0200

    use proper dependency injection in test suite

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,5 +1,6 @@
 import { LoggerService } from './logger.service';
 import { CalculatorService } from './calculator.service';
+import { TestBed } from '@angular/core/testing';
 
 describe('CalculatorService', () => {
   let calculator: CalculatorService;
@@ -7,7 +8,13 @@ describe('CalculatorService', () => {
 
   beforeEach(() => {
     loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
-    calculator = new CalculatorService(loggerSpy);
+    TestBed.configureTestingModule({
+      providers: [
+        CalculatorService,
+        { provide: LoggerService, useValue: loggerSpy },
+      ],
+    });
+    calculator = TestBed.inject(CalculatorService);
   });
 
   it('should add two numbers', () => {
