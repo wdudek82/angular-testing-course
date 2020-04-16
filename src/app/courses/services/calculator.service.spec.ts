d1e26495cb16c64aa75aa5e6f6b07b0e8f211ef0
Wojciech Dudek <wdudek82@gmail.com>
2020-04-16 21:04:20 +0200

    add method spies + mocked returned values (commented out)

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -4,14 +4,18 @@ import { CalculatorService } from './calculator.service';
 describe('CalculatorService', () => {
   it('should add two numbers', () => {
     // GIVEN
-    const loggerService = new LoggerService();
-    const calculator = new CalculatorService(loggerService);
+    const logger = jasmine.createSpyObj('LoggerService', ['log']);
+
+    // logger.log.and.returnValue('foo');
+
+    const calculator = new CalculatorService(logger);
 
     // WHEN
     const result = calculator.add(2, 3);
 
     // THEN
     expect(result).toBe(5);
+    expect(logger.log).toHaveBeenCalledTimes(1);
   });
 
   it('should subtract two numbers', () => {
@@ -23,6 +27,6 @@ describe('CalculatorService', () => {
     const result = calculator.subtract(3, 2);
 
     // THEN
-    expect(result).toBe(1);
+    expect(result).toBe(1, 'unexpected subtraction result');
   });
 });
